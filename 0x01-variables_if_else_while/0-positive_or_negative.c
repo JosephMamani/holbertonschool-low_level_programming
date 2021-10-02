afc42c9f9cb6d9c11f07a27c0576acd31c9fc77c
JosephMamani <jmmollenedo@gmail.com>
2021-10-02 00:09:30 +0000

    This task is about conditional to assign a random number to a variable which are positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints random number each time is executed*
+ * Return:0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("%d", n);
+	if (n > 0)
+	{
+		printf(" is negative\n");
+	}
+	else if (n > 0)
+	{
+		printf(" is positive\n");
+	}
+	else if (n == 0)
+	{
+		printf(" is zero\n");
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,2 @@
+ "This Project is about variables, conditional, if, else, while in C"
+ Here you can see several task using *conditionals* and *variables* about **C** programming
