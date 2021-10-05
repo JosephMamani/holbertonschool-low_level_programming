127df728302126e36ed5db21e121c663bf713d7a
JosephMamani <jmmollenedo@gmail.com>
2021-10-05 20:19:49 +0000

    Program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+int _putchar(char c);
+
+/**
+ * main - Prints "_putchar"
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	_putchar('_'); /* _putchar(95); */
+	_putchar(0x70); /* _putchar(112); */  /* _putchar('p'); */
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar(10); /* _putchar('\n'); */
+	return (0);
+}
