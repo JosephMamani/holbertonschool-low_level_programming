931c82ba11db5764d45a70a2881dd1d77fdd126b
JosephMamani <jmmollenedo@gmail.com>
2021-10-07 22:26:16 +0000

    Comment the line that is causing the infinite loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,11 +12,11 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-
-	while (i < 10)
-	{
-		putchar(i); /*After this line must be added the increment of the variable*/
-	}
+	/** while (i < 10)
+	* {
+	* 	putchar(i);
+	* }
+	*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
