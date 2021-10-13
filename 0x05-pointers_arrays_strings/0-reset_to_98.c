2cba7d42e6a2ef736dc3c44f4fc23d5ced362edb
JosephMamani <jmmollenedo@gmail.com>
2021-10-13 05:02:51 +0000

    Function that takes a pointers to an int

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Function that takes a pointer to an int
+ * @n: pointer to the variableto update
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+ "0x05. C - Pointers, arrays and strings"
+ On this project we will see tasks aboute pointers and variables
