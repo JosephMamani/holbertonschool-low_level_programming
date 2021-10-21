1b0cf2ce78b60191b7cf5ddf40b0649c84c19f11
JosephMamani <jmmollenedo@gmail.com>
2021-10-21 21:46:00 +0000

    function that returns 1 if the input integer is a prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,22 +1,21 @@
 #include "main.h"
-
 /**
- * is_prime_number: returns 1 if the input integer is a prime number
- * dividers_number: function to find the numbers of dividers
+ * dividers_number - numbers of dividers
+ * is_prime_number - give 1 if is a prime number
  * @n:the int to check
  * @c:counter
- * @i:initializer 
- * Return: 1 if c is lowercase, 0 otherwise
+ * @i:initializer
+ * Return: 1 if is prime, 0 if is not
  */
 
-int dividers_number (int n, int c, int i)
+int dividers_number(int n, int c, int i)
 {
 	if (i > n)
 		return (c);
 	if (n % i == 0)
 		return (dividers_number(n, c + 1, i + 1));
-	return (dividers_number (n, c, i + 1));
-}	
+	return (dividers_number(n, c, i + 1));
+}
 
 int is_prime_number(int n)
 {
