32b7b72da8804ed1eb64bae5fc04e23fca783f7a
JosephMamani <jmmollenedo@gmail.com>
2022-01-08 02:53:14 +0000

    function that adds a new node at the beginning of a dlistint_t list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,41 +1,31 @@
 #include "lists.h"
+
 /**
- * add_dnodeint -  adds a new node at the beginning of a dlistint_t list
- * @head: head of the list
- * @n: data inside the new node
- * Return: the address of the new element, or NULL if it failed
+ * add_dnodeint - Adds a new node at the beginning of a dlistint_t list.
+ * @head: Double pointer to struct.
+ * @n: data to insert in the new node.
+ *
+ * Return: The address of the new element, or NULL if it failed.
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new;
+	dlistint_t *node = NULL;
 
-	if (!head)
+	if (head == NULL)
 		return (NULL);
-	if (*head == NULL)
-	{
-		*head = malloc(sizeof(dlistint_t));
-			if (head == NULL)
-			{
-				dprintf(2, "Error: Can't malloc\n");
-				return (NULL);
-			}
-		(*head)->n = n;
-		(*head)->next = NULL;
-		(*head)->prev = NULL;
-		return (*head);
-	}
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
+	node = malloc(sizeof(dlistint_t));
+	if (!node)
 	{
 		dprintf(2, "Error: Can't malloc\n");
 		return (NULL);
 	}
-	new->n = n;
-	new->prev = NULL;
-	new->next = *head;
+	node->n = n;
+	node->next = *head;
+	node->prev = NULL;
 
-	(*head)->prev = new;
+	if (*head)
+		(*head)->prev = node;
 
-	*head = new;
-	return (new);
+	*head = node;
+	return (node);
 }
